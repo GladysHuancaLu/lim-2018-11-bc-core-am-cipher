78a4712fcd1b1da335bd77e0e4fbbc6a6aae701a
GladysHuancaLu <gladys.luana123@gmail.com>
2018-12-02 20:59:58 -0500

    Ahora al gh-pages

--- src/index.js
+++ src/index.js
@@ -21,32 +21,12 @@ comenzarResponse.addEventListener("click", () =>{
      document.getElementById("section2").style.display='block';
      document.getElementById("section1").style.display='none';
 });
-/*
-const btnLimpiar= document.getElementById("limpiar");
-btnLimpiar.addEventListener("click", () => {
-  const textoInicial= document.getElementById("textoACifrar");
-  textoInicial.value="";
-  const offset= document.getElementById("offset");
-  offset.value="";
-  textareaFinal.value="";
-});
-*/
+
 const btnErase= document.getElementById("limpiar");
 btnErase.addEventListener("click", () => {
     document.getElementById("formulario").reset();
 });
-/*
-const btnVolver= document.getElementById("volver");
-btnVolver.addEventListener("click", ()=> {
-  const textoInicial= document.getElementById("textoACifrar");
-  textoInicial.value="";
-  const offset= document.getElementById("offset");
-  offset.value="";
-  textareaFinal.value="";
-  document.getElementById("section2").style.display='none';
-  document.getElementById("section1").style.display='block';
-});
-*/
+
 const btnVolver= document.getElementById("volver");
 btnVolver.addEventListener("click", () => {
     window.location.href = 'index.html';
